fbebc91ff8f9a9f99394da02b156f0e1ea3982e8
Rosendoo <99772476+IamNotRose@users.noreply.github.com>
2024-07-17 13:48:18 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using AreaRetangulo;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AreaReangulo
+{
+
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Retangulo retangulo = new Retangulo();
+            Console.WriteLine("Calcule a area de um retangulo");
+            Console.Write("Informe a altura: ");
+            retangulo.Altura = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Informe a base: ");
+            retangulo.Base = Convert.ToDouble(Console.ReadLine());
+            retangulo.ExibirDados();
+            Console.ReadKey();
+
+
+        }
+
+    }
+}
